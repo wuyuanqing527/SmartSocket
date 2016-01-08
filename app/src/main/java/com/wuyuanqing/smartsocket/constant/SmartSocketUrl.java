13961af8ba448e6dec72394e683fc2bc3f3936df
wuyuanqing <wuyuanqing527@gmail.com>
2016-01-08 22:29:57 +0800

    引入了花生壳内网映射到外网

--- app/src/main/java/com/wuyuanqing/smartsocket/constant/SmartSocketUrl.java
+++ app/src/main/java/com/wuyuanqing/smartsocket/constant/SmartSocketUrl.java
@@ -4,7 +4,7 @@ package com.wuyuanqing.smartsocket.constant;
  * Created by wyq on 2015/11/19.
  */
 public class SmartSocketUrl {
-    private static String ip="http://192.168.1.207:8080";
+    private static String ip="http://yuanqingwu.6655.la:26076";
     public static  String getSocketListUrl =ip+"/SmartSocket/servlet/HomePage";
     public static  String loginUrl=ip+"/SmartSocket/servlet/LoginPage";
     public static  String registerUrl=ip+"/SmartSocket/servlet/RegisterPage";
