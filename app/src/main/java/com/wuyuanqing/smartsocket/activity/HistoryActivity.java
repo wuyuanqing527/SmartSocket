c2aaa2f4a6bed63c8104e82abf9976372baec049
yuanqingwu <yuanqingwu527@gmail.com>
2016-05-10 18:02:57 +0800

    修改percentBar传入实际值超过额定值时矩形超出长度的问题； 增加历史记录和定时设置按钮

---
+++ app/src/main/java/com/wuyuanqing/smartsocket/activity/HistoryActivity.java
@@ -0,0 +1,14 @@
+package com.wuyuanqing.smartsocket.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by Uni.W on 2016/5/10.
+ */
+public class HistoryActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/wuyuanqing/smartsocket/widget/PercentBar.java
+++ app/src/main/java/com/wuyuanqing/smartsocket/widget/PercentBar.java
@@ -17,7 +17,7 @@ public class PercentBar extends View {
     private String name = "name";
     private float maxNum = 100, minNum = 0, actuallyNum = 50;
     private int backBarColor = Color.GRAY;
-    private int barColor=Color.GREEN;
+    private int barColor = Color.GREEN;
 
     private Paint textPaint, rectPaint;
 
@@ -38,15 +38,16 @@ public class PercentBar extends View {
         this.name = name;
         this.maxNum = maxNum;
         this.minNum = minNum;
-        this.actuallyNum = actuallyNum;
+        this.actuallyNum = actuallyNum>maxNum?maxNum:actuallyNum;
         invalidate();
     }
 
     public void setBackBarColor(int color) {
         this.backBarColor = color;
     }
-    public void setBarColor(int color){
-        this.barColor=color;
+
+    public void setBarColor(int color) {
+        this.barColor = color;
     }
 
     @Override
@@ -54,6 +55,7 @@ public class PercentBar extends View {
         super.onDraw(canvas);
         textPaint = new Paint();
         textPaint.setColor(Color.BLACK);
+        textPaint.setAntiAlias(true);
         textPaint.setTextSize(30);
         //textPaint.setTextAlign(Paint.Align.CENTER);//水平居中
 
@@ -72,16 +74,17 @@ public class PercentBar extends View {
         int maxBseLine = (maxRect.top + maxRect.bottom - fontMetricsInt.top - fontMetricsInt.bottom) / 2;
         canvas.drawText(maxNum + "", (getWidth() - maxLength) / 2, maxBseLine, textPaint);
 
-//draw backBAR
+        //draw backBAR
         rectPaint = new Paint();
         rectPaint.setColor(backBarColor);
+        rectPaint.setAntiAlias(true);
         rectPaint.setStyle(Paint.Style.FILL);
         canvas.drawRect(0, perHight, getWidth(), getHeight() - perHight, rectPaint);
 
         //drawActuralBar
-        rectPaint=new Paint();
+        rectPaint = new Paint();
         rectPaint.setColor(barColor);
-        float top=(1-actuallyNum/(maxNum-minNum))*getHeight()*8/10+perHight;
-        canvas.drawRect(0,top,getWidth(),getHeight()-perHight,rectPaint);
+        float top = (1 - actuallyNum / (maxNum - minNum)) * getHeight() * 8 / 10 + perHight;
+        canvas.drawRect(0, top, getWidth(), getHeight() - perHight, rectPaint);
     }
 }
